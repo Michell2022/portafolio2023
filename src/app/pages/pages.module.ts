50b675d9c05f0cdff40da7dceb158c330b645d73
Michell2022 <sistemas.angel04@gmail.com>
2023-05-10 14:37:32 -0500

    modulos y componentes creados

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortafolioComponent,
+    ContactComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
