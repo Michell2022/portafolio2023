d8948e25cd24a69150e2aca9481e9d04dbd94c65
michell <116586325+Michell2022@users.noreply.github.com>
2023-10-10 00:56:43 -0500

    Actualizando mis servicios

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -10,15 +10,19 @@ export class ServicesComponent {
 
   servicios = [
     {
-      titulo: 'Desarrollador Web',
-      descripcion: 'Desarrollador web Front-End con habilidades en HTML, CSS, JavaScript y Angular. Ofrezco soluciones web personalizadas y de calidad, diseñadas para potenciar tu ',
-      textoExtra: 'presencia en línea y brindar una experiencia excepcional a tus usuarios.',
+      titulo: 'Desarrollador Web Full Stack',
+      descripcion: 'Como desarrollador web Full Stack, ofrezco soluciones integrales para tus proyectos en línea. Mis servicios incluyen: ' +
+                   '\n- Desarrollo Front End: Creación de interfaces atractivas y funcionales utilizando HTML, CSS y JavaScript, así como ',
+      textoExtra:  'frameworks como Angular. \n- Desarrollo Back End: Implementación de la lógica de servidor utilizando tecnologías como Java y Spring Boot, garantizando un rendimiento eficiente y una gestión segura de datos.' +  
+                   '\n- Bases de Datos: Diseño y gestión con MySQL.' +
+                   '\n- Diseño Responsivo: Experiencia óptima en dispositivos.' +
+                   '\n- Colaboración en Equipo: Trabajo conjunto para el éxito del proyecto.',
       icono: 'bx bx-code-alt'
     },
     {
       titulo: 'Servicio Técnico IT',
-      descripcion: 'Ofrezco servicios de mantenimiento preventivo para computadoras y laptops, asegurando un rendimiento óptimo y seguro. Además, realizo formateo e instalación de ',
-      textoExtra: 'sistemas operativos y programas personalizados según tus necesidades.',
+      descripcion: 'Ofrezco servicios de mantenimiento preventivo para computadoras y laptops, asegurando un rendimiento óptimo y seguro. Además, realizo formateo e instalación de sistemas operativos y',
+      textoExtra: 'programas personalizados según tus necesidades.',
       icono: 'bx bxl-windows'
     },
 
